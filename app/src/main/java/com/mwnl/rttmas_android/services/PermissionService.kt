833f06be56cb31fb59abf0bc8699cbe6ffe1e962
Henry C <yeahlowflicker@gmail.com>
2025-01-20 19:19:05 +0800

    Feature: Utility to obtain Android device ID
    This commit includes the getAndroidDeviceID() utility function to
    obtain the per-device unique ID.

--- app/src/main/java/com/mwnl/rttmas_android/services/PermissionService.kt
+++ app/src/main/java/com/mwnl/rttmas_android/services/PermissionService.kt
@@ -1,4 +1,24 @@
 package com.mwnl.rttmas_android.services
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
+
 class PermissionService {
+
+    @SuppressLint("HardwareIds")
+    /**
+     *  Obtain the Android device ID.
+     *  This ID is unique for each device.
+     *
+     *  @param  [Context] context    - pass the activity context here
+     *  @return [string] the obtained device ID
+     */
+    fun getAndroidDeviceID(context: Context) : String {
+        return Settings.Secure.getString(
+            context.contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+    }
+
 }
