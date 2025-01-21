4735c0644fada937f59e476c2499b547e1810ab6
Henry C <yeahlowflicker@gmail.com>
2025-01-21 21:35:33 +0800

    Feature: FCM service
    This version contains the FCM message handling logic.
    
    A new service declaration has been added to AndroidManifest.xml
    to allow the app to receive FCM events from the system.

--- app/src/main/java/com/mwnl/rttmas_android/services/FcmService.kt
+++ app/src/main/java/com/mwnl/rttmas_android/services/FcmService.kt
@@ -1,4 +1,35 @@
 package com.mwnl.rttmas_android.services
 
-class FcmService {
+import android.content.Intent
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class FcmService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        Log.d("RTTMAS", token)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d("RTTMAS", "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d("RTTMAS", "Message data payload: " + remoteMessage.data)
+
+            val broadcastIntent = Intent()
+            broadcastIntent.setAction("com.rttmas.FCM_DATA") // Custom action name
+            broadcastIntent.putExtra("message", remoteMessage.data["message"])
+            sendBroadcast(broadcastIntent) // Sends the broadcast with data
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(
+                "RTTMAS", "Message Notification Body: " + remoteMessage.notification!!
+                    .body
+            )
+        }
+    }
 }
