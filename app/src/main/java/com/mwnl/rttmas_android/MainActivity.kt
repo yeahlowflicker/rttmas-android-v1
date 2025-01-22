b16597f454ebc5a69edb572d81826ef02c65da79
Henry C <yeahlowflicker@gmail.com>
2025-01-22 15:45:54 +0800

    Update: Include RTTMAS object and GUI class in MainActivity

--- app/src/main/java/com/mwnl/rttmas_android/MainActivity.kt
+++ app/src/main/java/com/mwnl/rttmas_android/MainActivity.kt
@@ -1,35 +1,47 @@
 package com.mwnl.rttmas_android
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.mwnl.rttmas_android.core.GUI
+import com.mwnl.rttmas_android.core.RTTMAS
 import com.mwnl.rttmas_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var rttmas: RTTMAS
+
+
+    // GUI references
+    lateinit var gui : GUI
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
+        // Configure view binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
+        // Setup GUI references
+        gui = GUI(
+            findViewById(R.id.image_view_detection),
+            findViewById(R.id.switch_detection_status),
+            findViewById(R.id.text_detection_status),
+            findViewById(R.id.text_gps_latitude),
+            findViewById(R.id.text_gps_longitude),
+            findViewById(R.id.text_speed)
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+
+        // Initialize RTTMAS logic
+        rttmas = RTTMAS(this@MainActivity, this, gui)
+        rttmas.initialize()
+        rttmas.isDetectionEnabled = true
     }
+
+
 }
