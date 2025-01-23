fc5e3d756c7a198e37522a83ada2ca398735f942
Henry C <yeahlowflicker@gmail.com>
2025-01-23 13:02:11 +0800

    Feature: FCM service implementation
    The FCM subscription and message processing methods have been implemented and linked to the TrafficAlertManager class.

--- app/src/main/java/com/mwnl/rttmas_android/services/FcmService.kt
+++ app/src/main/java/com/mwnl/rttmas_android/services/FcmService.kt
@@ -1,22 +1,108 @@
 package com.mwnl.rttmas_android.services
 
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.mwnl.rttmas_android.core.TrafficAlertManager
+import org.json.JSONObject
 
-class FcmService : FirebaseMessagingService() {
+
+const val FCM_TOPIC_TRAFFIC_ALERTS = "alerts"
+
+
+class FcmService(
+    var activity: Activity,
+    var trafficAlertManager: TrafficAlertManager
+) : FirebaseMessagingService() {
+
+    private lateinit var receiver: BroadcastReceiver
+
+
+    /**
+     * Setup the entire FCM service and configure broadcast receivers.
+     * This listens to Android's system events to capture FCM messages.
+     */
+    fun setupFcm() {
+        // Setup broadcast receiver
+        // This allows the MainActivity to capture the received alert data
+        // from FCMService
+        // Define the BroadcastReceiver to handle the data from Service
+        receiver =
+            object : BroadcastReceiver() {
+                override fun onReceive(context: Context, intent: Intent) {
+                    // Get the message from the intent
+                    val message = intent.getStringExtra("message")
+
+                    Log.d(TAG, "Broadcast receiver received.")
+
+                    try {
+                        checkNotNull(message)
+
+                        val payload = JSONObject(message)
+                        trafficAlertManager.onTrafficAlertReceived(payload)
+
+                    } catch (e: Exception) {
+                        e.message?.let { Log.d(TAG, it) }
+                    }
+                }
+            }
+
+
+        // Register the receiver with the custom action
+        val filter = IntentFilter("com.rttmas.FCM_DATA")
+        activity.registerReceiver(receiver, filter, RECEIVER_NOT_EXPORTED)
+
+
+        // Subscribe to FCM topic(s)
+        subscribeToFcm()
+    }
+
+
+    /**
+     * Subscribe to FCM topic.
+     */
+    private fun subscribeToFcm() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new FCM registration token
+                val token = task.result
+
+                Log.d(TAG, token!!)
+            })
+
+        FirebaseMessaging.getInstance().subscribeToTopic(FCM_TOPIC_TRAFFIC_ALERTS)
+            .addOnCompleteListener { task ->
+                var msg = "FCM subscribed"
+                if (!task.isSuccessful)
+                    msg = "FCM subscribe failed"
+                Log.d(TAG, msg)
+            }
+    }
+
+
 
     override fun onNewToken(token: String) {
-        Log.d("RTTMAS", token)
+        Log.d(TAG, token)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d("RTTMAS", "From: " + remoteMessage.from)
+        Log.d(TAG, "From: " + remoteMessage.from)
 
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
-            Log.d("RTTMAS", "Message data payload: " + remoteMessage.data)
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
 
             val broadcastIntent = Intent()
             broadcastIntent.setAction("com.rttmas.FCM_DATA") // Custom action name
@@ -27,7 +113,7 @@ class FcmService : FirebaseMessagingService() {
         // Check if message contains a notification payload.
         if (remoteMessage.notification != null) {
             Log.d(
-                "RTTMAS", "Message Notification Body: " + remoteMessage.notification!!
+                TAG, "Message Notification Body: " + remoteMessage.notification!!
                     .body
             )
         }
